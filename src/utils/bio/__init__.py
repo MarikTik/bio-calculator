fa5fc3f53384bebdf22f56a4cc519f9ef981cff2
MarikTik <mtik.philosopher@gmail.com>
2024-10-23 21:26:36 -0700

    separated data related functionality into a subfolder and added a Codon class to manage codon counting more easily

---
+++ src/utils/bio/__init__.py
@@ -0,0 +1,1 @@
+from .codon import Codon
---
+++ src/utils/bio/codon.py
@@ -0,0 +1,49 @@
+from typing import Union, Dict
+
+class Codon:
+     def __init__(self, codon: str):
+          self.codon = codon.lower()
+
+     def __eq__(self, other: Union['Codon', str]) -> bool:
+          if isinstance(other, str):
+               return codon_to_amino_acid[self.codon] == codon_to_amino_acid[other.lower()]
+          elif isinstance(other, Codon):
+               return self.__eq__(other.codon)
+          raise TypeError(f"Cannot compare Codon with {type(other)}")
+
+     def __hash__(self) -> int:
+          return hash(codon_to_amino_acid[self.codon])
+     
+     @staticmethod
+     def codons_count_map(gene: str) -> Dict['Codon', int]:
+          triplets = [gene[i:i+3] for i in range(0, len(gene) - 2)]
+          codon_map = dict()
+          for triplet in triplets:
+               codon = Codon(triplet)
+               if codon in codon_map:
+                    codon_map[codon] += 1
+               else:
+                    codon_map[codon] = 1
+          return codon_map
+
+codon_associations = {
+    ("ttt", "ttc"): "Phenylalanine (Phe)",
+    ("tta", "ttg"): "Leucine (Leu)",
+    ("att", "atc", "ata"): "Isoleucine (Ile)",
+    ("atg",): "Methionine (Met)", 
+    ("gtt", "gtc", "gta", "gtg"): "Valine (Val)",
+    ("tct", "tcc", "tca", "tcg"): "Serine (Ser)",
+    ("cct", "ccc", "cca", "ccg"): "Proline (Pro)",
+    ("act", "acc", "aca", "acg"): "Threonine (Thr)",
+    ("gct", "gcc", "gca", "gcg"): "Alanine (Ala)",
+    ("tgg",): "Tryptophan (Trp)",
+    ("cgt", "cgc", "cga", "cgg"): "Arginine (Arg)"
+}
+
+ 
+codon_to_amino_acid = {
+    codon: name
+    for codons, name in codon_associations.items()
+    for codon in codons
+}
+
